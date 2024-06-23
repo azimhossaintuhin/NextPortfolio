157ce363afbbfaa0f6074bfc59c4cbf079eb6f52
Codecommerze <codecommerze@gmail.com>
2024-06-23 10:01:29 +0600

    updated some ui

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
     <section id='hero_section'>
     <div className="">
       <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
-        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        <div className="absolute inset-0  w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
         {/* Background components */}
         <Boxes />
 
