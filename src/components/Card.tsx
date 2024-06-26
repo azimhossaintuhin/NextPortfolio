fb951093fd656cde3b0e117637af6d085c11ff75
Codecommerze <codecommerze@gmail.com>
2024-06-26 07:21:43 +0600

    updated about and skills section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ const Card: React.FC<CardProps> = ({ title, description, image, counter }): JSX.
    
     <div className={`relative overflow-hidden border-2  px-2 py-2 border-emerald-400 rounded bg-gray-900 ${counter === 2 ? "col-span-1 md:col-span-2" : "col-span-1"}  shadow-lg md:mb-0 mb-4` }>
       <div className={`z-10 px-3 py-2 ${counter===2?" flex flex-col justify-normal md:items-center md:justify-center": ""}`}>
-      <Image src={image} alt="card image" width={60} height={60} className="rounded" />
+      <Image src={image} alt="card image" width={60} height={60} className="rounded z-50" />
       <h2 className="text-white text-xl font-bold mt-4">{title}</h2>
       <p className="text-white text-md mt-4">{description}</p>
       </div>
--- src/components/Home/Skills.tsx
+++ src/components/Home/Skills.tsx
@@ -1,103 +1,96 @@
-// importing third party  modeuls  or libararies 
-"use client"
-import { HoverEffect } from '@/components/ui/card-hover-effect'
-import { FaBootstrap , FaHtml5 , FaCss3 , FaJs , FaPython , FaReact   } from "react-icons/fa";
+// importing third party  modeuls  or libararies
+"use client";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import {
+  FaBootstrap,
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaPython,
+  FaReact,
+} from "react-icons/fa";
 import { BiLogoTailwindCss } from "react-icons/bi";
 import { DiJqueryLogo } from "react-icons/di";
 import { RiNextjsLine } from "react-icons/ri";
-import { SiDjango , SiRedis , SiFastapi} from "react-icons/si";
+import { SiDjango, SiRedis, SiFastapi } from "react-icons/si";
 
+const Skills: React.FC = (): JSX.Element => {
+  // // Define interface for skills
+  // interface Skill {
+  //     title: string;
+  //     icon : JSX.Element;
+  // }
 
-const  Skills: React.FC = ():JSX.Element => {
-    // // Define interface for skills
-    // interface Skill {
-    //     title: string;
-    //     icon : JSX.Element;
-    // }
+  const skills = [
+    {
+      title: <FaHtml5 className="text-5xl text-white text-center" />,
+      description: "HTML 5",
+    },
+    {
+      title: <FaCss3 className="text-5xl text-white" />,
+      description: "CSS 3",
+    },
+    {
+      title: <FaBootstrap className="text-5xl text-white" />,
+      description: "Bootstrap",
+    },
+    {
+      title: <BiLogoTailwindCss className="text-5xl text-white" />,
+      description: "Tailwind CSS",
+    },
+    {
+      title: <FaJs className="text-5xl text-white" />,
+      description: "Javascript",
+    },
+    {
+      title: <DiJqueryLogo className="text-5xl text-white" />,
+      description: "Jquery",
+    },
+    {
+      title: <FaReact className="text-5xl text-white" />,
+      description: "React JS",
+    },
+    {
+      title: <RiNextjsLine className="text-5xl text-white" />,
+      description: "Next JS",
+    },
+    {
+      title: <FaPython className="text-5xl text-white" />,
+      description: "Python",
+    },
+    {
+      title: <SiDjango className="text-5xl text-white" />,
+      description: "Django",
+    },
+    {
+      title: <SiFastapi className="text-5xl text-white" />,
+      description: "Fast Api",
+    },
+    {
+      title: <SiRedis className="text-5xl text-white" />,
+      description: "Redis",
+    },
+  ];
 
-    const skills = [
-        {
-            title:<FaHtml5 className='text-5xl text-white text-center'/>,
-            description :  "HTML 5",
-            link : '#'
-        },
-        {
-            title:<FaCss3 className='text-5xl text-white'/>,
-            description :  "CSS 3",
-            link : '#'
-        },
-        {
-            title:<FaBootstrap className='text-5xl text-white'/>,
-            description :  "Bootstrap",
-            link : '#'
-        },
-        {
-            title:<BiLogoTailwindCss className='text-5xl text-white'/>,
-            description :  "Tailwind CSS",
-            link : '#'
-        },
-        {
-            title:<FaJs className='text-5xl text-white'/>,
-            description :  "Javascript",
-            link : '#'
-        },
-        {
-            title:<DiJqueryLogo className='text-5xl text-white'/>,
-            description :  "Jquery",
-            link : '#'
-        },
-        {
-            title:<FaReact className='text-5xl text-white'/>,
-            description :  "React JS",
-            link : '#'
-        },
-        {
-            title:<RiNextjsLine className='text-5xl text-white'/>,
-            description :  "Next JS",
-            link : '#'
-        },
-        {
-            title:<FaPython className='text-5xl text-white'/>,
-            description :  "Python",
-            link : '#'
-        },
-        {
-            title:<SiDjango className='text-5xl text-white'/>,
-            description :  "Django",
-            link : '#'
-        },
-        {
-            title:<SiFastapi className='text-5xl text-white'/>,
-            description :  "Fast Api",
-            link : '#'
-        },
-        {
-            title:<SiRedis className='text-5xl text-white'/>,
-            description :  "Redis",
-            link : '#'
-        }
-        
-
-    ]
-
-    return(
-       <section id="skills" className="w-full h-dvh mt-72 px-2">
-        
-        <div className="container  ">
-            {/* content */}
-            <div className="flex justify-center flex-col items-center mb-20">
-                <h3 className="text-white text-5xl  font-bold ">My <span className="text-emerald-400">Skills</span></h3>
-                <p className="text-slate-100 font-semibold ">Here are my skills which I have working on for 4+ years</p>
-            </div>
-
-            <div className="max-w-5xl mx-auto px-8">
-      <HoverEffect items={skills} />
-    </div>
+  return (
+    <section id="skills" className="w-full h-dvh mt-15 px-2">
+      <div className="container  ">
+        {/* content */}
+        <div className="flex justify-center flex-col items-center mb-10">
+          <h3 className="text-white text-5xl  font-bold mb-3">
+            My <span className="text-emerald-400">Skills</span>
+          </h3>
+          <p className="text-slate-100 font-semibold">
+            Here are my skills which I have working on for 4+ years
+          </p>
         </div>
 
-       </section>
-    )
-}
+        <div className="max-w-5xl mx-auto px-8">
+          <HoverEffect items={skills} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
-export default Skills
+export default Skills;
