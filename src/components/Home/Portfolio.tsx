8ee469f46157dab6185c5d9842984349129c0f82
Codecommerze <codecommerze@gmail.com>
2024-07-07 21:21:12 +0600

    projects section is done

--- src/components/Home/Portfolio.tsx
+++ src/components/Home/Portfolio.tsx
@@ -5,12 +5,12 @@ import {DirectionAwareHoverDemo}  from '@/components/Project';
 // Portfolio Component
 const Portfolio: React.FC = (): JSX.Element => {
     return (
-        <section id="skills" className="w-full h-dvh mt-32 md:mt-5 px-2">
-            <div className="container px-24 ">
+        <section id="skills" className="w-full h-dvh mt-20 md:mt-5">
+            <div className=" w-full lg:px-32 px-4  flex justify-center items-center flex-col ">
                 {/* Portfolio */}
                 <div className="heading text-center mb-10">
-                    <h4 className="text-4xl font-extrabold text-white">My Portfolio</h4>
-                    <p className="text-gray-600">Here you can see all my portfolio projects that I have worked on.</p>
+                    <h4 className="text-4xl font-extrabold text-white">My <span className='text-emerald-500'>Portfolio</span></h4>
+                    <p className="text-gray-50">Here you can see all my portfolio projects that I have worked on.</p>
                 </div >
                 {/* content */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,24 @@
-
-import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
+import Image from "next/image";
 
 export function DirectionAwareHoverDemo() {
   const imageUrl =
     "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-   
-      <DirectionAwareHover imageUrl={imageUrl}>
-        <p className="font-bold text-xl">In the mountains</p>
-        <p className="font-normal text-sm">$1299 / night</p>
-      </DirectionAwareHover>
-  
+    <div className=" mx-auto p-4">
+      {/* image part */}
+      <div className="relative overflow-hidden group rounded-lg">
+        <Image src={imageUrl} alt="Picture of the author" layout="responsive" width={500} height={500} className="z-10 object-cover" />
+        {/* overlay */}
+        <div className="absolute inset-0 bg-emerald-200 opacity-0 group-hover:opacity-60 transition-opacity duration-500 z-20"></div>
+        {/* content part */}
+        <div className="absolute top-[-50%] left-2 group-hover:top-2 duration-500 transition-all z-30">
+          <p className="bg-emerald-500 text-white font-extrabold mb-4 rounded-full inline px-3 py-1 text-sm">CategoryName</p>
+        </div>
+        <div className="absolute bottom-[-50%] sm:bottom-[-70%] group-hover:bottom-2 z-30 transition-all duration-500 left-2">
+          <h1 className="text-lg sm:text-2xl font-bold text-white">Project Name</h1>
+          <p className="text-xs sm:text-sm text-white w-fit">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quos.</p>
+        </div>
+      </div>
+    </div>
   );
 }
